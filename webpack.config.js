7e502f68580964f618959a8594afac735633cdd4
“HamzA <hamza.khokhar660@gmail.com>
2018-09-13 11:48:22 -0600

    still configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+var path = require('path');
 
 module.exports = {
-    mode: "development",
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    // Add the JSHint loader
+    module: {
+        rules: [{
+            test: /\.src$/, // Run the loader on all .js files
+            exclude: /node_modules/, // ignore all files in the node_modules folder
+            use: 'jshint-loader'
+        }]
     }
 };
